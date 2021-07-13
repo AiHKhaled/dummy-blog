ca10c9022ae218811471ece23f3dfafd15f4f86b
parzivalElijah <aithammouk40@gmail.com>
2021-07-13 23:30:29 +0100

    added routes

--- app.js
+++ app.js
@@ -1,25 +1,71 @@
 const express = require("express");
-
+const Blog = require("./models/blog");
 const app = express();
+//connecting to mongoDB
+const mongoose = require("mongoose");
+const dbURI =
+  "mongodb+srv://elijah:1234@nodetuto.iwqfh.mongodb.net/node-tuto?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((error) => console.log(error));
+
 app.set("view engine", "ejs");
 
-app.listen(3000);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "this is a new blog",
+    body: "this is the body of the blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.get("/", (req, res) => {
-  const blogs = [
-    { title: "first blog", snippet: "lorem upsem jellk etc" },
-    { title: "second blog", snippet: "lorem upsem jellk etc" },
-    { title: "third blog", snippet: "lorem upsem jellk etc" },
-  ];
-  res.render("home", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find().sort({ createdAt: -1 });
+  Blog.find()
+    .then((result) => {
+      res.render("home", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create" });
 });
+
 app.use((req, res) => {
   res.status(404).render("404"), { title: "Error" };
 });
