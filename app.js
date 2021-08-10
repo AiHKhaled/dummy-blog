41f7814c80844a4400ac7033a7fa45c250404e87
parzivalElijah <aithammouk40@gmail.com>
2021-08-10 11:58:12 +0100

    done with the dummy blog

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const Blog = require("./models/blog");
+
 const app = express();
+const blogRoutes = require("./routes/blogRoutes");
+
 //connecting to mongoDB
 const mongoose = require("mongoose");
 const dbURI =
@@ -14,21 +16,6 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "new blog",
-    snippet: "this is a new blog",
-    body: "this is the body of the blog",
-  });
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
 
 app.get("/", (req, res) => {
   res.redirect("/blogs");
@@ -38,33 +25,7 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get("/blogs", (req, res) => {
-  Blog.find().sort({ createdAt: -1 });
-  Blog.find()
-    .then((result) => {
-      res.render("home", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create" });
-});
+app.use("/blogs", blogRoutes);
 
 app.use((req, res) => {
   res.status(404).render("404"), { title: "Error" };
